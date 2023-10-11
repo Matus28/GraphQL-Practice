207bb5db4425d145a2f85d8c552a41ce51f60281
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-11 11:50:50 +0200

    feat: add main index file with creating ApolloServer instance and  startStandaloneServer function

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// db
+import db from "./_db.js";
+
+// types
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  // typeDefs -- definitions of types of data => Schema (defines the shape of the graph)
+  typeDefs,
+  // resolvers
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("Server ready at port", 4000);
